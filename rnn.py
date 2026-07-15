9aa93f1585a24de908b7544fb36150fd7b90156e
pranav-vora <aceguitar2010@gmail.com>
2026-07-15 15:42:24 -0700

    added helper functions, train loop

--- rnn.py
+++ rnn.py
@@ -11,7 +11,7 @@ class RNN(nn.Module):
 
         self.hidden_size = 128
         self.input_size = N_LETTERS
-        self.output_size = 1
+        self.output_size = 2
 
 
         self.input2hidden = nn.Linear(self.input_size + self.hidden_size, self.hidden_size)
@@ -56,3 +56,72 @@ for i in range(line_tensor.size(0)):
 print(output)
 print(output.size())
 print(next_hidden.size())
+
+def category_from_output(output):
+    category_idx = torch.argmax(output).item()
+    return all_categories[category_idx]
+
+print(category_from_output(output))
+
+criterion = nn.NLLLoss()
+optimizer = torch.optim.SGD(rnn.parameters(), lr=0.005)
+
+def train(line_tensor, category_tensor):
+    hidden = rnn.init_hidden()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+
+    loss = criterion(output, category_tensor)
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    return output, loss.item()
+
+
+current_loss = 0
+all_losses = []
+plot_steps, print_steps = 1000, 5000
+
+n_iters = 5
+for i in range(n_iters):
+    category, line, category_tensor, line_tensor = random_training_example(category_lines, all_categories)
+    output, loss = train(line_tensor, category_tensor)
+    current_loss += loss
+
+    if (i+1) % plot_steps == 0:
+        all_losses.append(current_loss / plot_steps)
+        current_loss = 0
+
+    if (i+1) % print_steps == 0:
+        guess = category_from_output(output)
+        correct = "CORRECT" if guess == category else f"WRONG ({category})"
+        print(f"{i} {i/n_iters*100} {loss:.4f} {line} / {guess} {correct}")
+
+
+plt.figure()
+plt.plot(all_losses)
+plt.show()
+
+def predict(input_line):
+    print(f"\n> {input_line}")
+    with torch.no_grad():
+        line_tensor = line_to_tensor(input_line)
+
+        hidden = rnn.init_hidden()
+
+        for i in range(line_tensor.size()[0]):
+            output, hidden = rnn(line_tensor[i], hidden)
+
+        guess = category_from_output(output)
+        print(guess)
+
+
+while True:
+    sentence = input("Input:")
+    if sentence == "quit":
+        break
+
+    predict(sentence)
